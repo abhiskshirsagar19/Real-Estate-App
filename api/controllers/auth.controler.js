9bb445523a6b500abed4eed058245735b4bfd88c
abhikshirsagar19 <abhikshirsagar1999@gmail.com>
2024-04-07 16:54:30 +0530

    Sign in functionality added.

--- api/controllers/auth.controler.js
+++ api/controllers/auth.controler.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   //console.log(req.body);
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
   try {
